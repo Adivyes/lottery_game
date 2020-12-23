ca02350c3dd0645b768d8cd412eafbbdd2941a57
Adivyes <Adivyes@gmail.com>
2020-12-23 10:43:41 +0200

    init

--- promise.js
+++ promise.js
@@ -31,33 +31,33 @@
 // העליונה עובדת עם פרומיס רגיל עם ריג'קט וריסולב.
 // לעומת זאת הפונקציה התחתונה עושה את אותו הדבר עם אסינק ואוואית===================
 
-let counter =1;
- function ranNum(numberOfInput){
-    return new Promise((resolve,reject)=>{
-        let head = Math.floor((Math.random() * 10) + 1); 
-    console.log(head)
-        if(numberOfInput == head){
-            resolve(`nice work do it again` + ` ${head} 😀` )
+let counter = 1;
+function ranNum(numberOfInput) {
+    return new Promise((resolve, reject) => {
+        let head = Math.floor((Math.random() * 10) + 1);
+        console.log(head)
+        if (numberOfInput == head) {
+            resolve(`nice work do it again` + ` ${head} 😀`)
         }
 
-    reject ('you lost try agin 😭')
+        reject('you lost try agin 😭')
 
     })
 }
 
-async function ranNumbtn(){
-try{
-    let lotto = await ranNum(mainInput.value);
-    printToDiv(lotto)
-}
-catch (error){
-    printToDiv(error)
-}
-finally{
-    counterDiv.innerHTML = `<div><h3>numbers of trys: ${counter++}</h3></div>`
-}
+async function ranNumbtn() {
+    try {
+        let lotto = await ranNum(mainInput.value);
+        printToDiv(lotto)
+    }
+    catch (error) {
+        printToDiv(error)
+    }
+    finally {
+        counterDiv.innerHTML = `<div><h3>numbers of trys: ${counter++}</h3></div>`
+    }
 }
 
-function printToDiv(prams){
+function printToDiv(prams) {
     mainDiv.innerHTML = prams
 }
